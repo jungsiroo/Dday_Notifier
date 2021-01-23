c8d6914c42b71fc42badfd9d68e7138c7c1592e8
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2021-01-23 11:28:20 +0000

    Add splashscreen

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
 import * as React from 'react';
 import StartPage from './src/Components';
-import SplashScreen from 'react-native-splash-screen';
 
-export default class App extends React.Component {
-  componentDidMount() {
-    SplashScreen.hide();
-  }
-
-  render() {
-    return <StartPage />;
-  }
+export default function App() {
+  return <StartPage />;
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import InitialStack from './InitialStack';
+import SplashScreen from 'react-native-splash-screen';
 
-export default function StartPage() {
-  return (
-    <NavigationContainer>
-      <InitialStack />
-    </NavigationContainer>
-  );
+export default class StartPage extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <InitialStack />
+      </NavigationContainer>
+    );
+  }
 }
