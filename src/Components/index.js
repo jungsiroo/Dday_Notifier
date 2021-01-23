063d4dfbf9b6955f50623faae22568346d21b479
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2021-01-23 04:59:55 +0000

    Remove wrong import state

--- src/Components/index.js
+++ src/Components/index.js
@@ -11,11 +11,9 @@ import {
 } from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-// import Home from '../Screens/Home';
-// import Signup from '../Screens/Signup';
-// import LoginS from '../Screens/Login';
-
-import {Home, Signup, LoginS} from '../Screens';
+import Home from '../Screens/Home';
+import Signup from '../Screens/Signup';
+import LoginS from '../Screens/Login';
 
 const statusbarheight = StatusBar.currentHeight;
 const windowWidth = Dimensions.get('window').width;
