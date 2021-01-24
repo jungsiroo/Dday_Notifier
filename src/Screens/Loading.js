e8e4fe3aca923c2681880905cbcdf74e0eef8250
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2021-01-24 10:16:36 +0000

    Testing

--- src/Screens/Loading.js
+++ src/Screens/Loading.js
@@ -3,22 +3,22 @@ import auth from '@react-native-firebase/auth';
 import SplashScreen from 'react-native-splash-screen';
 
 function IsUserLogined() {
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
+  // const [initializing, setInitializing] = useState(true);
+  // const [user, setUser] = useState();
 
-  // Handle user state changes
-  function onAuthStateChanged(User) {
-    setUser(user);
-    if (initializing) setInitializing(false);
-  }
+  // // Handle user state changes
+  // function onAuthStateChanged(User) {
+  //   setUser(user);
+  //   if (initializing) setInitializing(false);
+  // }
 
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+  //   return subscriber; // unsubscribe on unmount
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
-  if (initializing || !user) return false;
+  // if (initializing || !user) return false;
 
   return true;
 }
