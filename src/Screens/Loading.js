651e8d8d1a96567d35933e2f46b04dcfac210663
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2021-01-24 11:09:12 +0000

    Apply async & await to Loading.js

--- src/Screens/Loading.js
+++ src/Screens/Loading.js
@@ -2,34 +2,34 @@ import React, {useState, useEffect} from 'react';
 import auth from '@react-native-firebase/auth';
 import SplashScreen from 'react-native-splash-screen';
 
-function IsUserLogined() {
-  // const [initializing, setInitializing] = useState(true);
-  // const [user, setUser] = useState();
+async function IsUserLogined() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-  // // Handle user state changes
-  // function onAuthStateChanged(User) {
-  //   setUser(user);
-  //   if (initializing) setInitializing(false);
-  // }
+  // Handle user state changes
+  function onAuthStateChanged(User) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
 
-  // useEffect(() => {
-  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-  //   return subscriber; // unsubscribe on unmount
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  // if (initializing || !user) return false;
+  if (initializing || !user) return false;
 
   return true;
 }
 
 export default class LoadingPage extends React.Component {
-  componentDidMount() {
-    this.props.navigation.navigate(IsUserLogined() ? 'Home' : 'Login');
+  async componentDidMount() {
+    this.props.navigation.navigate((await IsUserLogined()) ? 'Home' : 'Login');
 
-    // setTimeout(() => {
-    //   SplashScreen.hide();
-    // }, 1500);
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1500);
   }
 
   render() {
