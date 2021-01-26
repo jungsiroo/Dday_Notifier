9898a777f493271933b823e4ebda21a7907cc58d
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2021-01-26 10:35:13 +0000

    Make logout function

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,16 +1,19 @@
-import * as React from 'react';
+import React, {useContext} from 'react';
 import {Button, Text, View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import DetailsScreen from './Detail';
+import {AuthContext} from '../Components/AuthProvider';
+
+function Logout() {
+  const {logout} = useContext(AuthContext);
+  logout();
+}
 
 function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Home screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+      <Button title="Logout" onPress={() => Logout()} />
     </View>
   );
 }
