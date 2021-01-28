d5539c175c11070127c9f0a87deff6d1aecf196f
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2021-01-28 10:06:25 +0000

    Test Toast Message

--- src/Components/AuthProvider.js
+++ src/Components/AuthProvider.js
@@ -1,40 +1,57 @@
 import React, {createContext} from 'react';
 import auth from '@react-native-firebase/auth';
 import {useState} from 'react';
+import Toast from 'react-native-toast-message';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
   const [user, setUser] = useState(null);
 
+  const ToastMsgHandler = (status, Error) => {
+    Toast.show({
+      type: 'info',
+      position: 'top',
+      visibilityTime: 6000,
+      autoHide: true,
+      topOffset: 70,
+      bottomOffset: 40,
+      text1: status + ' Error',
+      text2: Error,
+    });
+  };
+
   return (
-    <AuthContext.Provider
-      value={{
-        user,
-        setUser,
-        login: async (email, password) => {
-          try {
-            await auth().signInWithEmailAndPassword(email, password);
-          } catch (e) {
-            console.log(e);
-          }
-        },
-        register: async (email, password) => {
-          try {
-            await auth().createUserWithEmailAndPassword(email, password);
-          } catch (e) {
-            console.log(e);
-          }
-        },
-        logout: async () => {
-          try {
-            await auth().signOut();
-          } catch (error) {
-            console.log(error);
-          }
-        },
-      }}>
-      {children}
-    </AuthContext.Provider>
+    <>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
+      <AuthContext.Provider
+        value={{
+          user,
+          setUser,
+          login: async (email, password) => {
+            try {
+              await auth().signInWithEmailAndPassword(email, password);
+            } catch (e) {
+              ToastMsgHandler('Login', e);
+            }
+          },
+          register: async (email, password) => {
+            try {
+              await auth().createUserWithEmailAndPassword(email, password);
+            } catch (e) {
+              ToastMsgHandler('Signup', e);
+            }
+          },
+          logout: async () => {
+            try {
+              await auth().signOut();
+            } catch (error) {
+              ToastMsgHandler('Logout');
+            }
+          },
+        }}>
+        {children}
+      </AuthContext.Provider>
+    </>
   );
 };
