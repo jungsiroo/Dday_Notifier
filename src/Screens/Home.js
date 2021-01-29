0ff94036ec51eb8b264fbe6d4643c99d66e2e040
Sihyun Jung <54366260+alpha-src@users.noreply.github.com>
2021-01-29 10:25:59 +0000

    Add Toast Handler in Home Screen

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,15 +1,37 @@
 import React, {useContext} from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
 import {AuthContext} from '../Components/AuthProvider';
+import Toast from 'react-native-toast-message';
+import {useEffect} from 'react';
 
 const HomeScreen = () => {
+  const ToastMsgHandler = (user) => {
+    Toast.show({
+      type: 'info',
+      position: 'top',
+      visibilityTime: 6000,
+      autoHide: true,
+      topOffset: 70,
+      bottomOffset: 40,
+      text1: 'Login Successed',
+      text2: 'Welcome' + user.toString(),
+    });
+  };
+
   const {user, logout} = useContext(AuthContext);
 
+  useEffect(() => {
+    ToastMsgHandler();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome {user.uid}</Text>
-      <Button title="Logout" onPress={() => logout()} />
-    </View>
+    <>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome {user.uid}</Text>
+        <Button title="Logout" onPress={() => logout()} />
+      </View>
+    </>
   );
 };
 
